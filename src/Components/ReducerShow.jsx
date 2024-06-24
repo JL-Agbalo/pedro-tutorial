33234e499bd59b5b0ed12c39a3243ca2f557e4d6
fishagbalo <fishagbalo@gmail.com>
2024-06-24 22:15:36 +0800

    ading onclick

--- src/Components/ReducerShow.jsx
+++ src/Components/ReducerShow.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function ReducerShow() {
+    const [count, setCount] = useState(0)
+    const [showText, setShowText] = useState(true)
   return (
-    <div>ReducerShow</div>
+    <div>
+        <h1>{count}</h1>
+        <button onClick={() => {
+            setCount(count+1)
+            setShowText(!showText);
+        }}>
+            Click Here
+
+        </button>
+        {showText && <p>This is a Text</p>}
+    </div>
   )
 }
 
